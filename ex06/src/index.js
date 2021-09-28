f7954ab8d4026184cc0a6e2fba025756268de3fc
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-28 06:16:35 +0200

    .

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -1,1 +1,11 @@
+// Only change code below this line
 
+const arena = () => {
+    // Only change code above this line
+    
+    'use strict';
+    return new Date;
+    };
+    
+    console.log(arena());
+    module.exports = arena;
