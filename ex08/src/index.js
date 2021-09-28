629382f32aeacf7181cbc9b2adda56e9e808e3f5
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-28 06:20:24 +0200

    .

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,1 +1,12 @@
+// Only change code below this line
 
+const sum = (num1, num2) =>{
+
+    if (num2 === undefined) num2=1;
+
+return num1 + num2;
+}
+
+// Only change code above this line
+
+console.log(sum(3));
