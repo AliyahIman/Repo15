e7e146129df72fe4a56749cd7ed356217e07af79
AliyahIman <90403246+AliyahIman@users.noreply.github.com>
2021-09-28 06:05:49 +0200

    .

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,1 +1,21 @@
+const milili = [10, 25, 4]; // DO NOT change this line
 
+function myArray(mili) 
+{
+    'use strict';
+
+    // Only change code below this line
+    // Using mili = [4 , 10, 25] would be invalid
+   let a = mili[0];
+   let b = mili[1];
+   let c = mili[2];
+   mili[0] = c;
+   mili[1] = a;
+   mili[2] = b;
+
+    //Only change code above this line
+
+    return mili;
+}
+console.log(myArray(milili));
+module.exports= myArray;
